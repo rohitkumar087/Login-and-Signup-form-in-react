043d99f683af35db841a9425296ae78f4a5e3a65
rohit kumar <rohitkumarprajapat087@gmail.com>
2025-07-30 10:58:41 +0530

    Update form

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -6,11 +6,28 @@ export default function SignUp() {
     let [name, setName] = useState('');
     let [email, setEmail] = useState('');
     let [phone, setPhone] = useState('');
-    let [password, setPassword] = useState('');
+    let [password, setPassword] = useState();
+    let [error,setError]=useState("");
 
      function handleSubmit(e) {
         e.preventDefault();
-        const result = {
+       
+        // if(!name&& !email && !phone&& !password){
+        //     setError("Please fill all the fields");
+        //     return;
+        // }
+        const allError ={}
+        if(!name) allError.name = 'Name is required'
+        if(!email) allError.email = 'Email is required'
+        if(!phone) allError.phone = 'Phone is required'
+        if(!password) allError.password = 'Password is required'
+        
+        if(Object.keys(allError).length>0){
+            setError(allError);
+            return;
+        }
+
+         const result = {
             name, email,phone,password
         }
 
@@ -29,35 +46,50 @@ export default function SignUp() {
             <div className='formStyle'>
                 <h1>Sign up</h1>
                 <form onSubmit={handleSubmit}>
-                    <input
+                    <div>
+                        <input
                         type="text"
                         placeholder='Enter your name'
                         value={name}
                         onChange={e => setName(e.target.value)}
                     />
+                {error.name && <p style={{color:"white",textAlign:"left"}}>{error.name}</p>}
+                    </div>
                     <br />
-                    <input
+                   <div>
+                     <input
                         type="email"
                         placeholder='Email'
                         value={email}
                         onChange={e => setEmail(e.target.value)}
                     />
+                                    {error.email && <p style={{color:"white",textAlign:"left"}}>{error.email}</p>}
+
+                   </div>
                     <br />
 
-                    <input
+                   <div>
+                     <input
                         type="number"
                         placeholder='Phone number'
                         value={phone}
                         onChange={e => setPhone(e.target.value)}
                     />
+                                    {error.phone && <p style={{color:"white",textAlign:"left"}}>{error.phone}</p>}
+
+                   </div>
                     <br />
 
-                    <input
+                   <div>
+                     <input
                         type="password"
                         placeholder='Password'
                         value={password}
                         onChange={e => setPassword(e.target.value)}
                     />
+                                    {error.password && <p style={{color:"white",textAlign:"left"}}>{error.password}</p>}
+
+                   </div>
                     <br />
 
                     <button>Sign up</button>
