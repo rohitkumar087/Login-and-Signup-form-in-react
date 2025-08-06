d349e6b0c5ba0d38f46b7628961d4633a5e8a6ba
rohit kumar <rohitkumarprajapat087@gmail.com>
2025-08-06 11:02:34 +0530

    condition rendering

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -2,36 +2,53 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Nav from "./Nav"
 
-export default function Fetch(){
-    const [data,setData] = useState("")
-    const [toggle,setToggle] = useState(true)
+export default function Fetch() {
+    const [data, setData] = useState([])
+    const [toggle, setToggle] = useState(true)
+    const [loading, setLoading] = useState(true)
 
     const navigate = useNavigate()
-const fetchData = () =>{
-    fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(result => setData(result))
-    console.log(data)
-}
-const fetchToggle = () =>{
-    setToggle(!toggle)
-    if(toggle){
-        navigate('/')
+    const fetchData = () => {
+        setLoading(true)
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(result => setData(result))
+        console.log(data)
+        setLoading(false)
     }
-    else{
-        navigate('/fetch')
+    const fetchToggle = () => {
+        setToggle(!toggle)
+        if (toggle) {
+            navigate('/')
+        }
+        else {
+            navigate('/fetch')
+        }
     }
-}
 
-useEffect(()=>{
-    fetchData()
-},[toggle])
+    useEffect(() => {
+        fetchData()
+    }, [])
 
-    return(
+    return (
         <>
-        <Nav/>
-        <h1>Fetch data</h1>
-        <button onClick={fetchToggle}>Change</button>
+            <Nav />
+            <h1>Fetch data</h1>
+            {/* <button onClick={fetchToggle}>Change</button> */}
+
+            {loading ? (<h2>Loading....</h2>) :
+                (<div style={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
+                    {data.map((item) => (
+                        <div key={item.id} style={{ border: "1px solid gray", padding: "10px", margin: "10px", width:"350px" }}>
+                            <img src={item.image} alt={item.title} style={{ width: "100px" }} />
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                            <p><strong>Price:</strong> ${item.price}</p>
+                        </div>
+                    ))}
+                </div>
+                )}
+
         </>
     )
 }
